1c71f4fd9ecf317a1e3b1aa3863e93c749921089
Bohdan_Smolenskyi <bohdan_smolenskyi@epam.com>
2015-12-07 12:29:30 +0200

    initial commit, added game and comment entity

---
+++ OnlineGameStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineGameStore.MvcApplication" Language="C#" %>
---
+++ OnlineGameStoreData/Entities/GameEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineGameStoreData.Entities
+{
+    public class GameEntity
+    {
+        [Key]
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
